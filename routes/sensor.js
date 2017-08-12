aff86ad6f5b9e5eea205c2c400d20ebab8003967
Michael Kirsch <michael.kirsch@jambit.com>
2017-08-12 14:22:25 +0200

    Implement lastChanged

--- routes/sensor.js
+++ routes/sensor.js
@@ -6,12 +6,27 @@ router.get('/', function(req, res, next) {
 
     gpio.setup(7, gpio.DIR_IN, readInput);
 
+    var previousValue = false;
+    var timestampLastChange = 0;
+
     function readInput() {
-        gpio.read(7, function(err, value) {
-            console.log('The value is ' + value);
+        gpio.read(7, function(err, currentValue) {
+            console.log('The value is ' + currentValue);
+
+            if(previousValue != currentValue){
+                previousValue = currentValue;
+                timestampLastChange = getCurrentTimeInSeconds();
+            }
+
+            var lastChange = getCurrentTimeInSeconds() - timestampLastChange;
             res.status(200);
-            res.send("Value is " + value);
+            res.send({"currentState" : currentValue, "lastChanged" : lastChange});
+
         });
+    }
+
+    function getCurrentTimeInSeconds() {
+        return Math.round(new Date() / 1000);
     }
 });
 
